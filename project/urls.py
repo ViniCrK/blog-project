c3f64064663eb5093bc0261efb5ed307f657a67b
ViniCrK <vinicrk@outook.com>
2024-02-29 11:35:28 -0300

    Serving static files

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
