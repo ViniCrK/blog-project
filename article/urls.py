5ab942426d00c1d198e07722f97920d55a2761d6
ViniCrK <vinicrk@outook.com>
2024-03-01 20:07:05 -0300

    Add route to reference to article id

--- article/urls.py
+++ article/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_articles)
+    path('<int:id>/', views.detail_article, name='detail_article'),
+    path('', views.list_articles),
 ]
