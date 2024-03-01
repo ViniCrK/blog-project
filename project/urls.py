c52189de41ce0be9e13b2883ae0d725ea2fdd894
ViniCrK <vinicrk@outook.com>
2024-03-01 19:14:18 -0300

    Change include parameter to reference article app

--- project/urls.py
+++ project/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('', include('article.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
