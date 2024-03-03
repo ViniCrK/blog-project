c1f294123da08033720abc1e8bcc9625649dd9a6
ViniCrK <vinicrk@outlook.com>
2024-03-03 18:39:28 -0300

    Adjust routes

--- article/urls.py
+++ article/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('article/<int:id>/', views.detail_article),
-    path('list_articles/', views.list_articles),
+    path('article/<int:id>/', views.detail_article, name='detail_article'),
+    path('', views.list_articles, name='list_articles'),
 ]
