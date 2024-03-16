766ef2b30f22707303c1900587e7a8f7fb54d43d
ViniCrK <vinicrk@outlook.com>
2024-03-16 10:34:03 -0300

    Create model Category

--- article/models.py
+++ article/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=100)
