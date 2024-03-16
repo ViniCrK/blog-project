a0421b56d17771800003bf0421fa3be223fff82f
ViniCrK <vinicrk@outlook.com>
2024-03-16 10:48:59 -0300

    Update __str__ to show name of category only

--- article/models.py
+++ article/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
