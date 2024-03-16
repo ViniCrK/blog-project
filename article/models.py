dff05e4a29d19867009387fa304075d2576694a4
ViniCrK <vinicrk@outlook.com>
2024-03-16 10:37:23 -0300

    Add field category to article

--- article/models.py
+++ article/models.py
@@ -9,3 +9,8 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=100)
     content = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category, models.SET_NULL, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.title}'
