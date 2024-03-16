482f3cfb1802bf73068346d11128819bb8ffb607
ViniCrK <vinicrk@outlook.com>
2024-03-16 10:49:43 -0300

    Add settings for category and article

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from article import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class CateogryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
+
+
+@admin.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title',
+    ordering = '-id',
+    search_fields = 'title', 'subtitle',
+    list_per_page = 10
+    list_display_links = 'id', 'title',
