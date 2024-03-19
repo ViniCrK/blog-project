8de301189cf66d115d4336602bde31f109e182c0
ViniCrK <vinicrk@outlook.com>
2024-03-19 19:15:21 -0300

    Create model Tag and Add tag tags to Article

--- article/models.py
+++ article/models.py
@@ -8,12 +8,17 @@ class Category(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=254)
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     category = models.ForeignKey(
         Category, models.SET_NULL, blank=True, null=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self) -> str:
         return f'{self.title}'
