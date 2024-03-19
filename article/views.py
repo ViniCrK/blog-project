6b44dd4e466ead77734c7dff1f13610f1a2ad0e4
ViniCrK <vinicrk@outlook.com>
2024-03-19 20:01:37 -0300

    Deleted article list that had the articles inside, but now the articles will come from database

--- article/views.py
+++ article/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
+from .models import Article
 
-articles = [
-    {
-        'id': 1,
-        'category': 'F1',
-        'title': "Max Verstappen wins Bahrain Grand Prix amid Red Bull turmoil",
-        'subtitle': "It was a commanding one-two win for Red Bull with teammate Sergio Perez",
-        'content': "SAKHIR, Bahrain — The ongoing drama around Red Bull had no effect on its three-time reigning world champion as Max Verstappen routed the field by 22 seconds in the season-opening Formula 1 race on Saturday. Verstappen started the Bahrain Grand Prix on pole position and was never seriously challenged on his way to a commanding one-two win for Red Bull with teammate Sergio Perez. Embattled team principal Christian Horner was joined before the race and during the podium celebration by his former pop star wife, Geri Halliwell.",
-    },
-    {
-        'id': 2,
-        'category': 'Eletric Cars',
-        'title': "The politics and economics behind Biden's China-car espionage probe",
-        'subtitle': "The administration is throwing out some scary scenarios",
-        'content': "WASHINGTON — President Joe Biden, vowing to 'do right by U.S. autoworkers', launched an investigation this week into whether Chinese-made vehicles could be used to spy on Americans, a far-off threat given the few such cars on U.S. roads now. The White House announced the probe Thursday citing national-security risks about 'connected' cars creating 'new avenues for espionage and sabotage.'",
-    },
-]
+# articles = [
+#     {
+#         'id': 1,
+#         'category': 'F1',
+#         'title': "Max Verstappen wins Bahrain Grand Prix amid Red Bull turmoil",
+#         'subtitle': "It was a commanding one-two win for Red Bull with teammate Sergio Perez",
+#         'content': "SAKHIR, Bahrain — The ongoing drama around Red Bull had no effect on its three-time reigning world champion as Max Verstappen routed the field by 22 seconds in the season-opening Formula 1 race on Saturday. Verstappen started the Bahrain Grand Prix on pole position and was never seriously challenged on his way to a commanding one-two win for Red Bull with teammate Sergio Perez. Embattled team principal Christian Horner was joined before the race and during the podium celebration by his former pop star wife, Geri Halliwell.",
+#     },
+#     {
+#         'id': 2,
+#         'category': 'Eletric Cars',
+#         'title': "The politics and economics behind Biden's China-car espionage probe",
+#         'subtitle': "The administration is throwing out some scary scenarios",
+#         'content': "WASHINGTON — President Joe Biden, vowing to 'do right by U.S. autoworkers', launched an investigation this week into whether Chinese-made vehicles could be used to spy on Americans, a far-off threat given the few such cars on U.S. roads now. The White House announced the probe Thursday citing national-security risks about 'connected' cars creating 'new avenues for espionage and sabotage.'",
+#     },
+# ]
 
 
 def list_articles(request):
     context = {
-        'articles': articles
+        'articles': Article.objects.all()
     }
 
     return render(request, 'index.html', context)
@@ -28,6 +29,6 @@ def list_articles(request):
 
 def detail_article(request, id):
     context = {
-        'article': articles[id - 1],
+        'article': Article.objects.get(pk=id),
     }
     return render(request, 'detail_article.html', context)
