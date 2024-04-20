04d66eb3dd1cf4c74f8d1d4cbf38f1dac322402f
ViniCrK <vinicrk@outlook.com>
2024-04-20 12:56:29 -0300

    Change the views name

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from users import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
