c48a8f228f62aeb1a66def77e3d49f334af02e93
ViniCrK <vinicrk@outlook.com>
2024-04-23 16:01:42 -0300

    Add and import login_required decorator

--- article/views.py
+++ article/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Article
 
 
+@login_required
 def list_articles(request):
     articles = Article.objects.all()
 
